241bc5198fc577cb20df321cf6d3b1d439aac6a4
jayfortier79 <jfortier05@yahoo.com>
2023-08-22 23:59:15 -0500

    first commit was monday

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+const axios = require("axios");
 
-function App() {
+
+
+
+export default function app () {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='App'>
+         <body>
+  <header>
+    <h1>Sartre's List</h1>
+    <h2>Better-DressedPeople</h2> 
+    </header>
+    </body>
+      </div>
+  )
 }
 
-export default App;
