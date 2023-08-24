c93e5a3037eb50f8cdf034bbad40ca7a3981855c
jayfortier79 <jfortier05@yahoo.com>
2023-08-24 05:54:48 -0500

    api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-const axios = require("axios");
+import axios from 'axios';
+import Page from './components/page'
+
+
 
 
 
 
 export default function app () {
-    
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+  console.log(process.env.REACT_APP_API_KEY);
+
+  let category = 'happiness'
+  $.ajax({
+      method: 'GET',
+      url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+      headers: { 'X-Api-Key': apiKey},
+      contentType: 'application/json',
+      success: function(result) {
+          console.log(result);
+      },
+      error: function ajaxError(jqXHR) {
+          console.error('Error: ', jqXHR.responseText);
+      }
+  });
+   
   return (
       <div className='App'>
          <body>
@@ -13,6 +33,8 @@ export default function app () {
     <h2>Better-DressedPeople</h2> 
     </header>
     </body>
+    <Page/>
+    <p>{Page}</p>
       </div>
   )
 }
---
+++ src/components/quote.js
@@ -0,0 +1,16 @@
+
+
+
+let category = 'happiness'
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    headers: { 'X-Api-Key': apiKey},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+});
